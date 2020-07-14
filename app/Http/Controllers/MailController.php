2f155d231fe86694d7847f3af5aa18a652cd8a9a
Developer Prodigi <devprodigi2020@gmail.com>
2020-07-14 15:35:26 +0700

    new first project on prodigi

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    public function sendEmailResetPassword()
+    {
+        $to_name = "RECEIVER_NAME";
+        $to_email = "RECEIVER_EMAIL_ADDRESS";
+        $data = array("name"=>"Ogbonna Vitalis(sender_name)", 'body' => 'A test mail');
+        Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) {
+        $message->to($to_email, $to_name)
+        ->subject('Laravel Test Mail');
+        $message->from('SENDER_EMAIL_ADDRESS','Test Mail');
+    });
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
